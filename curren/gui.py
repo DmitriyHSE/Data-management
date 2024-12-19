31aa89b15c48feb53de7c3f5246ab99ebf131f8f
Masterfar7 <145840792+Masterfar7@users.noreply.github.com>
2024-12-19 20:46:31 +0300

    Update gui.py

--- curren/gui.py
+++ curren/gui.py
@@ -31,20 +31,39 @@ class main_window(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.connect_button.clicked.connect(self.show_start)
         #self.delete_button.clicked.connect(self.delete_by_FIO)  #
         self.delete_database_button.clicked.connect(self.delete_database)  #
-        self.schedule_table = ["Lesson id", "Teacher id", "Group id", "День недели", "Корпус/онлайн", "Номер аудитории", "Вид занятий", "Время"]
+        self.schedule_column = ["Lesson id", "Teacher id", "Group id", "День недели", "Корпус/онлайн", "Номер аудитории", "Вид занятий", "Время"]
+        self.disciplines_column = ["ID дисциплины", "Дисциплины"]
+        self.schedule_table.itemChanged.connect(self.update_schelude)
+        #self.schedule_table.setHorizontalHeaderLabels(self.schedule_column)
+        self.edit_flag = False
 
     def show_start(self):
         self.connectionWindow.show()
 
     def connect(self, name, user, password, host, port):
         self.db = Database(name, user, password, host, port)
+        try:
+            self.data_schedule = self.db.get_schedule()
+            self.set_data(self.schedule_table,self.schedule_column,self.data_schedule)
+            self.schedule_table.setRowCount(1)
+            #self.set_data(self.person_table, self.columns_persons, self.data_persons)
+            #self.set_data(self.department_table, self.columns_departments, self.data_departments)
+        except Exception as ex:
+            print(traceback.format_exc())
+            self.message("Error during connect!", traceback.format_exc())
+    def message(self, error, detailed_error="idk", icon=QMessageBox.Warning):
+        msg = QMessageBox()
+        msg.setWindowTitle("Отчёт")
+        msg.setIcon(icon)
+        msg.setText(f"{error}")
+        msg.setDetailedText(detailed_error)
+        msg.addButton(QMessageBox.Ok)
+        msg.exec()
     def delete_database(self):
         try:
             if self.db is not None:
                 self.db.delete_database()
-                self.schedule_table=[]
-                self.set_data(self.department_table, self.columns_departments, self.data_departments)
-                self.set_data(self.person_table, self.columns_persons, self.data_persons)
+                self.set_data(self.schedule_table,self.schedule_column,self.data_schedule)
                 self.db = None
                 self.connectionWindow = None
                 self.connectionWindow = startWindow(self)
@@ -66,3 +85,28 @@ class main_window(QtWidgets.QMainWindow, design.Ui_MainWindow):
         except Exception as ex:
             self.message("Error during setting data!", traceback.format_exc())
         self.edit_flag = False
+
+    def update_schelude(self, item):
+        if not self.edit_flag:
+            try:
+                print(item)
+                if item.column() == 1:
+                    print(item.text())
+                    self.db.update_schedule_by_lesson_id(item.text(),self.schedule_table.item(item.row(), 0).text())
+                elif item.column() == 2:
+                    self.db.update_schedule_by_teacher_id(item.text(), self.schedule_table.item(item.row(), 0).text())
+                    print("teacher")
+                elif item.column() == 3:
+                    self.db.update_schedule_by_group_id(item.text(), self.schedule_table.item(item.row(), 0).text())
+                elif item.column() == 4:
+                    self.db.update_schedule_by_day_of_the_week(item.text(), self.schedule_table.item(item.row(), 0).text())
+                elif item.column() == 5:
+                    self.db.update_schedule_by_korpus(item.text(), self.schedule_table.item(item.row(), 0).text())
+                elif item.column() == 6:
+                    self.db.update_schedule_by_number_of_auditoria(item.text(), self.schedule_table.item(item.row(), 0).text())
+                elif item.column() == 7:
+                    self.db.update_schedule_by_lesson_time(item.text(), self.schedule_table.item(item.row(), 0).text())
+                self.data_schedule = self.db.get_schedule()
+                self.set_data(self.schedule_table, self.schedule_column, self.data_schedule)
+            except Exception:
+                self.message("Error during data update!", traceback.format_exc())
